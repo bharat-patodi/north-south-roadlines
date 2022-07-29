257ee493666eda9111de1c0d856f7a58f4076be2
Bharat Patodi <bharatpatodi@gmail.com>
2022-07-29 13:22:11 +0530

    add contact details

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,6 +7,9 @@ const Contact = () => {
       <Header />
       <main>
         <h1>Contact Us</h1>
+        <p>Address: 21/7, Gulab Bagh, Dewas Naka, Indore, Madhya Pradesh, India</p>
+        <p>Phone Number: 94254-10665, 92001-61925, 98931-33019</p>
+        <p>Email: northsouthrl@gmail.com</p>
       </main>
       <Footer />
     </>
